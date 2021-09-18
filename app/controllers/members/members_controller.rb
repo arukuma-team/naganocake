f96fdb67c843bf9e3a99c4c4fbea4ff83fe52285
EC2 Default User <ec2-user@ip-172-31-52-66.ec2.internal>
2021-09-18 08:03:20 +0000

    new_member_registration & edit_member_registration

--- app/controllers/members/members_controller.rb
+++ app/controllers/members/members_controller.rb
@@ -1,2 +1,25 @@
 class Members::MembersController < ApplicationController
+    
+  def edit
+    @member = Member.find(params[:id])
+      if @member != current_user
+         redirect_to edit_members_member_path(current_user.id)
+      end
+  end
+  
+  def update
+    @member = Member.find(params[:id])
+      if @member.update(member_params)
+        redirect_to members_member_path(current_user.id)
+      else
+        render :edit
+      end
+  end
+    
+end
+
+private
+
+def member_params
+  params.require(:member).permit(:lastname, :firstname, :lastruby, :firstruby, :zip, :address, :tel, :email)
 end
