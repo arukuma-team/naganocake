a140fdd028367c71a3114d4f41cbb11a4337ad1c
Tonko27 <tonkotsu4127@gmail.com>
2021-09-21 10:01:24 +0000

    [Add]create/categories

--- app/controllers/admins/categories_controller.rb
+++ app/controllers/admins/categories_controller.rb
@@ -1,2 +1,42 @@
 class Admins::CategoriesController < ApplicationController
+  
+  def index
+    @categories = Category.all
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admins_categories_path(@category), notice: "カテゴリーを追加しました"
+    else
+      @categories = Category.all
+      render 'admins/categories/index'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admins_categories_path, notice: "カテゴリーを変更しました"
+    else
+      render "admins/categories/edit"
+    end
+  end
+  
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to admins_categories_path, notice: "カテゴリーを削除しました"
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:category)
+  end
 end
