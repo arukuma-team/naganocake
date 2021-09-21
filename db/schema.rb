edba7cfa03b7b0f23e973590164c8cb00bc80ecc
KazumaYoshie <yosh1kazu217629@gmail.com>
2021-09-21 06:21:49 +0000

    [Add]items/input

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_053249) do
+ActiveRecord::Schema.define(version: 2021_09_21_060842) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "member_id"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2021_09_17_053249) do
     t.text "description"
     t.string "price"
     t.boolean "status"
-    t.string "image"
+    t.string "item_ima"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
