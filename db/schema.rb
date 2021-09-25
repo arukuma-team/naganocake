6812863f358c8bb3f85f3889362eea6448eda34a
KazumaYoshie <yosh1kazu217629@gmail.com>
2021-09-25 04:51:33 +0000

    [Fix]member/status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_055333) do
+ActiveRecord::Schema.define(version: 2021_09_25_043048) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "member_id"
